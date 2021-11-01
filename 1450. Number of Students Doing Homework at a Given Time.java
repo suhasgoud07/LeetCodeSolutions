5b356ed353c9e594fd9587a1c5d0a4b2abfd52e7
Suhas V S <76420047+suhasgoud07@users.noreply.github.com>
2021-11-01 09:49:14 +0530

    Add files via upload

---
+++ 1450. Number of Students Doing Homework at a Given Time.java
@@ -0,0 +1,17 @@
+ 
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count=0;
+        for(int i=0;i<startTime.length;i++)
+        {
+            for(int j=startTime[i];j<=endTime[i];j++)
+            {
+                if(j==queryTime)
+                {
+                    count++;
+                }
+            }
+        }return count;
+        
+    }
+}
